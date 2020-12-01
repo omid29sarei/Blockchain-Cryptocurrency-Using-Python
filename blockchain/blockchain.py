dc3c263f22dd0e1601e9244a4cc5ca3a179d7c0c
omid <osarei@truststamp.net>
2020-12-01 08:45:41 +0100

    until mining everything is working

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,5 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify,request
 from time import time
+from flask_cors import CORS
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+from collections import OrderedDict
+import binascii
+from uuid import uuid4
+
+
+MINING_SENDER = 'The Blockchain'
+MINING_REWARD = 1
+
 class Blockchain:
 
 
@@ -10,6 +22,7 @@ class Blockchain:
         """
         self.transaction = []
         self.chain = []
+        self.node_id = str(uuid4()).replace('-','')
         #creating the genesis block
         self.create_block(0,'00')
     def create_block(self,nonce,previous_hash):
@@ -25,16 +38,111 @@ class Blockchain:
         # Reset the transaction
         self.transaction =[]
         self.chain.append(block)
+        return block
+    
+    def verify_transaction_signature(self,sender_public_key,signature,transaction):
+        public_key = RSA.importKey(binascii.unhexlify(sender_public_key))
+        verifier = PKCS1_v1_5.new(public_key)
+        hashed_public_key = SHA.new(str(transaction).encode('utf-8'))
+        try:
+            verifier.verify(hashed_public_key,binascii.unhexlify(signature))
+            return True
+        except ValueError:
+            return False
+
+    def submit_transaction(self,sender_public_key,recipient_pubic_key,signature,amount):
+    
+        transaction = OrderedDict({
+            'sender_public_key':sender_public_key,
+            'recipient_public_key':recipient_pubic_key,
+            'amount':amount
+        })
+
+         #TODO: Reward The Miner
+        #reward the mining
+        if(sender_public_key == MINING_SENDER):
+            self.transaction.append(transaction)
+            return len(self.chain) + 1
+        else:
+            #TODO: Signature Validation
+
+            #transaction from wallet to wallet
+            signature_verification = self.verify_transaction_signature(sender_public_key,signature,transaction)
+            if(signature_verification):
+                self.transaction.append(transaction)
+                return len(self.chain) + 1
+            else:
+                return False
+    def proof_of_work(self):
+        return 12345
+    
+    def hash(self,block):
+        return 'abc'
+        
         
 #initialize the Blockchain Class
 blockchain = Blockchain()
 
 #initialize the Node
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return render_template('./index.html')
+
+@app.route('/transactions/get',methods=['GET'])
+def get_transaction():
+    transaction = blockchain.transaction
+    response = {'transactions':transaction}
+    return jsonify(response),200
+
+
+@app.route('/mine',methods=['GET'])
+def mine():
+    #We Run The Proof Of Work Algorithm 
+    nonce = blockchain.proof_of_work()
+
+    blockchain.submit_transaction(sender_public_key=MINING_SENDER,
+                                  recipient_pubic_key=blockchain.node_id,
+                                  signature= '',
+                                  amount= MINING_REWARD )
+
+    last_block = blockchain.chain[-1]
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.create_block(nonce,previous_hash)
+    response = {
+                'message': 'New Block Created',
+                'block_number':block['block_number'],
+                'transactions':block['transaction'],
+                'nonce':block['nonce'],
+                'previous_hash':block['previous_hash']
+
+            }
+    return jsonify(response),200
+
+@app.route('/transaction/new',methods=['POST'])
+def new_transaction():
+    values = request.form
+
+    #check the vrequired field
+    required_attr = ['confirmation_sender_public_key','confirmation_recipient_public_key','transaction_signature','confirmation_amount']
+    if not all(k in values for k in required_attr):
+        return 'Missing Values',400
+
+    transaction_result = blockchain.submit_transaction(values['confirmation_sender_public_key'],
+                                                       values['confirmation_recipient_public_key'],
+                                                       values['transaction_signature'],
+                                                       values['confirmation_amount'])
+
+    if(transaction_result == False):
+        response = {'message':'Invalid Transaction!!'}
+        return jsonify(response), 406
+    else:
+        response = {'message':'Transaction Will Be Added To The Block' + str(transaction_result)}
+        return jsonify(response), 201
+
+    
 
 
 if __name__ == '__main__':
--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -1,17 +1,30 @@
-from flask import Flask,render_template, jsonify
+from flask import Flask,render_template, jsonify,request
 import Crypto
 import Crypto.Random
 from Crypto.PublicKey import RSA
 import binascii
+from collections import OrderedDict
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
 class Transaction:
 
-    def __init__(self,sender_address,sender_private_key,receipient_address,value):
-        self.sender_address = sender_address
+    def __init__(self,sender_public_key,sender_private_key,receipient_address,value):
+        self.sender_public_key = sender_public_key
         self.sender_private_key = sender_private_key
         self.receipient_address = receipient_address
         self.value = value
 
-
+    def to_dict(self):
+        return OrderedDict({
+            'sender_public_key':self.sender_public_key,
+            'recipient_public_key':self.receipient_address,
+            'amount': self.value
+        })
+    def sign_transaction(self):
+        private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
+        signer = PKCS1_v1_5.new(private_key)
+        h = SHA.new(str(self.to_dict()).encode('utf8'))
+        return binascii.hexlify(signer.sign(h)).decode('ascii')
 app = Flask(__name__)
 
 
@@ -22,6 +35,20 @@ def index():
 @app.route("/make/transaction")
 def make_transaction():
     return render_template('./make_transaction.html')
+
+@app.route("/generate/transaction", methods=['POST'])
+def generate_transaction():
+    sender_public_key = request.form['sender_publickey']
+    sender_private_key = request.form['sender_privatekey']
+    recipient_public_key = request.form['reccipient_publickey']
+    amount = request.form['amount']
+
+    transaction = Transaction(sender_public_key,sender_private_key,recipient_public_key,amount)
+
+    response = {'transaction':transaction.to_dict(),
+                'signature':transaction.sign_transaction()}
+
+    return jsonify(response),200
 
 
 @app.route("/view/transaction")
